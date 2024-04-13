a4e5b602b2df01517a337a63d2533c319c3228b6
EKhobotin <evgen.29.3.85@gmail.com>
2024-04-13 12:19:37 +0300

    fix

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,42 +2,62 @@ import * as contactsService from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
 
 export const getAllContacts = async (req, res) => {
-  const result = await contactsService.listContacts();
-  res.json(result);
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const getOneContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await contactsService.getContactById(id);
-  if (!result) {
-    throw HttpError(404);
+  try {
+    const { id } = req.params;
+    const result = await contactsService.getContactById(id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
   }
-  res.json(result);
 };
 
 export const deleteContact = async (req, res) => {
-  const { id } = req.params;
-  const result = await contactsService.removeContact(id);
-  if (!result) {
-    throw HttpError(404);
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
   }
-  res.json(result);
 };
 
 export const createContact = async (req, res) => {
-  const { name, email, phone } = req.body;
-  const result = await contactsService.addContact(name, email, phone);
-  res.status(201).json(result);
+  try {
+    const { name, email, phone } = req.body;
+    const result = await contactsService.addContact(name, email, phone);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const updateContact = async (req, res) => {
-  if (Object.keys(req.body).length === 0) {
-    throw HttpError(400, "Body must have at least one field");
+  try {
+    if (Object.keys(req.body).length === 0) {
+      throw HttpError(400, "Body must have at least one field");
+    }
+    const { id } = req.params;
+    const result = await contactsService.updateContact(id, req.body);
+    if (!result) {
+      throw HttpError(404);
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
   }
-  const { id } = req.params;
-  const result = await contactsService.updateContact(id, req.body);
-  if (!result) {
-    throw HttpError(404);
-  }
-  res.status(200).json(result);
 };
